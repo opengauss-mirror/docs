e98fc79719b12ac7d5747b5a728f6abae13fe5aa
wu <wucaiping@h-partners.com>
2023-02-17 03:56:24 +0000

    update sphinx/source/conf.py.
    Signed-off-by: wu <wucaiping@h-partners.com>

--- sphinx/source/conf.py
+++ sphinx/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2022, openGauss'
 author = 'openGauss'
 
 # The full version, including alpha/beta/rc tags
-release = 'latest'
+release = '3.1.0'
 
 
 # -- General configuration ---------------------------------------------------
@@ -62,6 +62,7 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_style = 'css/my_theme.css'
 
 source_parsers = {
     '.md': 'recommonmark.parser.CommonMarkParser',
