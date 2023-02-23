06bd5c9ad1bb6122eb32cc512ba39f5ee716c73b
pxq <1456889284@qq.com>
2023-02-23 20:00:48 +0800

    fix:语言切换功能、版本切换功能优化、部分冗余代码剔除

--- static/js/init-theme.js
+++ static/js/init-theme.js
@@ -1,14 +1,15 @@
 // 预览网站的初始化地址
-const preInitUrlObj={
-  urlZh:"/zh/docs/latest/docs/Releasenotes/%E6%B3%95%E5%BE%8B%E5%A3%B0%E6%98%8E.html",
-  urlEn:"/en/docs/latest/docs/Releasenotes/Terms-of-Use.html",
-}
+const preInitUrlObj = {
+  urlZh:
+    "/zh/docs/latest/docs/Releasenotes/%E6%B3%95%E5%BE%8B%E5%A3%B0%E6%98%8E.html",
+  urlEn: "/en/docs/latest/docs/Releasenotes/Terms-of-Use.html",
+};
 // 官网网站的初始化地址
-const initUrlObj={
-  urlZh:"/zh/docs/3.1.1/docs/BriefTutorial/BriefTutorial.html",
-  urlEn:"/en/docs/3.1.1/docs/BriefTutorial/BriefTutorial.html",
-}
-function initUrl(urlObj){
+const initUrlObj = {
+  urlZh: "/zh/docs/3.1.1/docs/BriefTutorial/BriefTutorial.html",
+  urlEn: "/en/docs/3.1.1/docs/BriefTutorial/BriefTutorial.html",
+};
+function initUrl(urlObj) {
   const lang=location.pathname
   if (lang === "/zh/") {
     window.open(urlObj.urlZh, "_self");
@@ -16,10 +17,11 @@ function initUrl(urlObj){
     window.open(urlObj.urlEn, "_self");
   }
 }
+// 因为存在一个最新版本文档的预览网站也是使用本仓库代码构建，所以这里针对预览网站做处理(特殊处理仅这一处)
 if (location.origin === "https://opengauss-docs-preview.test.osinfra.cn") {
-  initUrl(preInitUrlObj)
+  initUrl(preInitUrlObj);
 } else {
-  initUrl(initUrlObj)
+  initUrl(initUrlObj);
 }
 const themeStyle = localStorage.getItem("openeuler-theme");
 const html = document.getElementsByTagName("html")[0];
