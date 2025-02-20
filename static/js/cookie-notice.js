0d8403c4e66773065a58872909afb8aeaaf0ece3
zjwmiao <1723168479@qq.com>
2025-02-20 16:21:28 +0800

    fix: cookie政策跳转链接

--- static/js/cookie-notice.js
+++ static/js/cookie-notice.js
@@ -118,7 +118,7 @@ $(function () {
         ? "我们在本网站上使用Cookie，包括第三方Cookie，以便网站正常运行和提升浏览体验。单击“全部接受”即表示您同意这些目的；单击“全部拒绝”即表示您拒绝非必要的Cookie；单击“管理Cookie”以选择接受或拒绝某些Cookie。需要了解更多信息或随时更改您的 Cookie 首选项，请参阅我们的 "
         : 'This site uses cookies from us and our partners to improve your browsing experience and make the site work properly. By clicking "Accept All", you consent to the use of cookies. By clicking "Reject All", you disable the use of unnecessary cookies. You can manage your cookie settings by clicking "Manage Cookies". For more information or to change your cookie settings, please refer to our',
       cookie: !isEn ? "《关于cookies》。" : "About Cookies.",
-      cookieHref: !isEn ? "/cookies" : "/cookies/en",
+      cookieHref: `https://opengauss.org${!isEn ? "/cookies" : "/cookies/en"}`,
       action: [
         {
           btn: "全部接受",
