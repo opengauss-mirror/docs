2e4491233ca8a1dd1e2ce215278cf875cafc616b
zjwmiao <1723168479@qq.com>
2025-03-27 14:14:11 +0800

    fix: 拒绝cookie后清除百度统计相关资源

--- static/js/analytics.js
+++ static/js/analytics.js
@@ -1,11 +1,3 @@
-// 百度统计
-(function () {
-  var hm = document.createElement('script');
-  hm.src = 'https://hm.baidu.com/hm.js?ab8d86daab9a8e98cf8faa239aefcd3c';
-  var s = document.getElementsByTagName('script')[0];
-  s.parentNode.insertBefore(hm, s);
-})();
-
 window.dataLayer = window.dataLayer || [];
 function gtag() {
   window.dataLayer.push(arguments);
